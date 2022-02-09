aa8c23efbd3f50bd0b973fa04b409a624a6687ee
Gent Jegeni <geentjegeni@gmail.com>
2022-02-09 22:55:32 +0100

    tst

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 import Add from "./Add";
 
 const Landing = () => {
-    return (
-        <div className="landing-page">
-           <Add/>
-        </div>
-    )
-}
+  return (
+    <div className="landing-page">
+      <Add />
+      <div id="BrzyHr_app"></div>
+      <script src="https://parks-rec.breezy.hr/embed/js?bzsrc=jswidget&include_filters=true&no_pos_msg=true"></script>
+    </div>
+  );
+};
 
 export default Landing;
